2ccb84edb9f866247d1d5dd0eef0db91e050ac17
DarkyEG <usf.kh.kh@gmail.com>
2022-11-14 20:10:18 +0200

    feat(ClientOptions): add bot option
    Make the client work with bots and users

--- packages/discord.js/src/util/Options.js
+++ packages/discord.js/src/util/Options.js
@@ -17,6 +17,7 @@ const { toSnakeCase } = require('./Transformers');
  * @property {number|number[]|string} [shards] The shard's id to run, or an array of shard ids. If not specified,
  * the client will spawn {@link ClientOptions#shardCount} shards. If set to `auto`, it will fetch the
  * recommended amount of shards from Discord and spawn that amount
+ * @property {boolean} [bot=true] Determine the type of client
  * @property {number} [closeTimeout=5_000] The amount of time in milliseconds to wait for the close frame to be received
  * from the WebSocket. Don't have this too high/low. Its best to have it between 2_000-6_000 ms.
  * @property {number} [shardCount=1] The total amount of shards used by all processes of this bot
